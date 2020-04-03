8287a5887f39cf4e759f4b7018d68a6a9eeda470
c0ld Z3r0 <55325340+c0ld-z3r0@users.noreply.github.com>
2020-04-03 17:43:55 +0530

    New array input and print with pointers

---
+++ n_element_store_array_with_pointers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int n,val;
+    printf("Enter total number of elements you want to store in the array : ");
+    scanf("%d",&n);
+    int arr[n];
+    for (int a = 0 ; a < n ; a++)
+    {
+        val = 0;
+        printf("Enter %d element : ",a);
+        scanf("%d",arr+a);
+        printf("\n");
+    }
+    printf("The array elements are : \n");
+    for (int b = 0 ; b < n ; b++)
+    {
+        printf("%d  ",*(arr+b));
+    }
+    return 0;
+}
